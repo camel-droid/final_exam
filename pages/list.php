f8e754cf39efd9d47e6e20317a500fbcd12aea7a
camel-droid <camel-droid@gmail.com>
2021-01-27 16:01:28 +0000

    Task-B using associated array

--- pages/list.php
+++ pages/list.php
@@ -1,3 +1,42 @@
+<?php
+// データベース接続情報文字列
+$dsn = "mysql:host=localhost;dbname=productdb;charset=utf8";
+$user = "productdb_admin";
+$password = "admin123";
+// データベース接続関連オブジェクトの初期化
+$pdo = null;
+$pstmt = null;
+/**
+ * データベースからの全件検索
+ */
+try {
+  // データベースに接続
+  $pdo = new PDO($dsn, $user, $password);
+  // 実行するSQLを設定
+  $sql = "select * from product";
+  // SQL実行オブジェクトを取得
+  $pstmt = $pdo->prepare($sql);
+  // SQLの実行と結果セットの取得
+  $pstmt->execute();
+  $records = $pstmt->fetchAll(PDO::FETCH_ASSOC);
+  // 結果セットを商品の配列に格納
+  $products = [];
+  foreach ($records as $record) {
+    $product["id"] = $record["id"];
+    $product["category"] = $record["category"];
+    $product["name"] = $record["name"];
+    $product["price"] = $record["price"];
+    $product["detail"] = $record["detail"];
+    $products[] = $product;
+  }
+} catch (PDOException $e) {
+  echo $e->getMessage();
+  die;
+} finally {
+  unset($pstmt);
+  unset($pdo);
+}
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -11,6 +50,7 @@
 </header>
 <main id="list">
 	<h2>商品一覧</h2>
+	<?php if (count($products) > 0): ?>
 	<table class="list">
 		<tr>
 			<th>商品ID</th>
@@ -19,6 +59,22 @@
 			<th>価格</th>
 			<th></th>
 		</tr>
+		<?php foreach ($products as $product): ?>
+		<tr>
+			<td><?= $product["id"] ?></td>
+			<td><?= $product["category"] ?></td>
+			<td><?= $product["name"] ?></td>
+			<td>&yen;4100</td>
+			<td class="buttons">
+				<form name="inputs">
+					<input type="hidden" name="id" value="<?= $product["id"] ?>" />
+					<button formaction="update.php" formmethod="post" name="action" value="update">更新</button>
+					<button formaction="confirm.php" formmethod="post" name="action" value="delete">削除</button>
+				</form>
+			</td>
+		</tr>
+		<?php endforeach; ?>
+		<!--
 		<tr>
 			<td>1</td>
 			<td>財布・小物入れ</td>
@@ -123,7 +179,9 @@
 				</form>
 			</td>
 		</tr>
+		-->
 	</table>
+	<?php endif; ?>
 </main>
 <footer>
 	<div id="copyright">&copy; 2021 The Applied Course of Web System Development.</div>
