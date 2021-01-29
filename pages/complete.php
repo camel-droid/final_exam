be6a62c08edeb77ca989b24fc0467f488a1f5d1d
villageworks-tutor <villageworks-tutor@gmail.com>
2021-01-29 21:39:46 +0900

    edit complete.php

--- pages/complete.php
+++ pages/complete.php
@@ -10,50 +10,18 @@ isset($_REQUEST["action"]) ? $action = $_REQUEST["action"] : $action = "";
 // セッションの取得
 session_start();
 $product = $_SESSION["product"];
+// 処理の実行
+$dao = new ProductDao();
+// 処理の切り替え
+if ($action === "entry") {
+  $dao->inesert($product);
+} elseif ($action === "update") {
+  $dao->update($product);
+} else {
+  $dao->delete($product->getId());
+}
 // セッションの破棄
 unset($_SESSION["product"]);
-$dao = new ProductDao();
-try {
-  // データベース接続オブジェクトを取得
-  $pdo = connectDB();
-  // リクエストパラメータのactionキーによって処理を分岐
-  if ($action === "entry") {
-    /* 新規追加処理 */
-    $dao->insert($product);
-  } elseif ($action === "update") {
-    /* 更新処理 */
-    // 実行するSQLを設定
-    $sql = "update product set category = :category, name = :name, price = :price, detail = :detail where id = :id";
-    // リクエストパラメータをプレースホルダに設定する連想配列の設定
-    $params = [];
-    $params[":id"] = $product->getId();
-    $params[":category"] = $product->getCategory();
-    $params[":name"] = $product->getName();
-    $params[":price"] = $product->getPrice();
-    $params[":detail"] = $product->getDetail();
-    // SQl実行オブジェクトを取得
-    $pstmt = $pdo->prepare($sql);
-    // SQLを実行
-    $pstmt->execute($params);
-  } elseif ($action === "delete") {
-    /* 削除処理 */
-    // 実行するSQLを設定
-    $sql = "delete from product where id = ?";
-    // SQl実行オブジェクトを取得
-    $pstmt = $pdo->prepare($sql);
-    // プレースホルダにリクエストパラメータを設定
-    $pstmt->bindValue(1, $product->getId());
-    // SQLを実行
-    $pstmt->execute();
-  }
-  
-} catch (PDOException $e) {
-  echo $e->getMessage();
-  die;
-} finally {
-  unset($pstmt);
-  unset($pdo);
-}
 ?>
 <!DOCTYPE html>
 <html lang="ja">
